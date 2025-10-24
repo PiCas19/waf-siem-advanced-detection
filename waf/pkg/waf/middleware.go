df755b44386dce690d145d0627d89d16c34d6c09
Pierpaolo Casati <pierpaolo.casati@student.supsi.ch>
2025-10-24 20:01:39 +0200

    added: all files

--- waf/pkg/waf/middleware.go
+++ waf/pkg/waf/middleware.go
@@ -10,8 +10,8 @@ import (
 	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
 	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
 
-	"github.com/PiCas19/waf-siem-advanced-detection/waf/internal/detector"
-	"github.com/PiCas19/waf-siem-advanced-detection/waf/internal/logger"
+	"waf/internal/detector"
+	"waf/internal/logger"
 )
 
 func init() {
