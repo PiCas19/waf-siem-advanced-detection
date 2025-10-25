d611b647f064dab574e175efbb035acf715335f7
Pierpaolo Casati <pierpaolo.casati@student.supsi.ch>
2025-10-25 22:22:34 +0200

    fixed: stats page, created interceptor for JWT token in local storage

--- dashboard/src/services/api.ts
+++ dashboard/src/services/api.ts
@@ -4,6 +4,15 @@ const api = axios.create({
   baseURL: '/api',
 });
 
+// Add token to all requests
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('authToken');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
 export const fetchStats = () => api.get('/stats').then(res => res.data);
 export const fetchRules = () => api.get('/rules').then(res => res.data);
 export const fetchLogs = () => api.get('/logs').then(res => res.data);
