c049ce4749fe038d6ddff0dd3e3bb99e863657bd
Pierpaolo Casati <pierpaolo.casati@student.supsi.ch>
2025-11-21 15:46:20 +0100

    fix: Make LogJSON write to file instead of stdout
    LogJSON was using log.Println which writes to stdout instead of the
    log file. Now it opens/closes files on each write to match the Log
    method pattern and properly persists events to waf_*.log files.
    
    Also handle the error return value in handler.go.

--- waf/internal/logger/json_formatter.go
+++ waf/internal/logger/json_formatter.go
@@ -2,10 +2,34 @@ package logger
 
 import (
 	"encoding/json"
-	"log"
+	"os"
+	"path/filepath"
 )
 
-func (l *Logger) LogJSON(event map[string]interface{}) {
-	data, _ := json.Marshal(event)
-	log.Println(string(data))
+func (l *Logger) LogJSON(event map[string]interface{}) error {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+
+	// Marshal to JSON
+	data, err := json.Marshal(event)
+	if err != nil {
+		return err
+	}
+
+	// Create directory if it doesn't exist (in case it was deleted)
+	dir := filepath.Dir(l.filename)
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+
+	// Open file, write, and close (handles file recreation if deleted)
+	f, err := os.OpenFile(l.filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	// Write to file
+	_, err = f.Write(append(data, '\n'))
+	return err
 }
